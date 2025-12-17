05705a6033457171e502d2585a4e3b2dd2580f25
Anna Becchi <lanna.becchi@gmail.com>
2025-12-17 11:29:52 +0100

    Add example for unsat chc system

---
+++ examples/unsat_check.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from pychc.solvers.witness import Status
+from pychc.solvers.golem import GolemSolver, ProofFormat
+from pychc.solvers.opensmt import OpenSMTSolver
+from pychc.chc_system import CHCSystem
+
+from pysmt.typing import INT, REAL
+from pysmt.logics import QF_UFLRA, QF_UFLIA
+from pysmt.shortcuts import (
+    And,
+    Symbol,
+    Equals,
+    Int, Real,
+    Plus, Minus,
+    FALSE,
+    LT,
+)
+from pychc.shortcuts import Predicate, Apply
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+sys = CHCSystem(logic=QF_UFLIA)
+
+# Create a predicate inv(x: Int)
+inv = Predicate("inv", [INT])
+sys.add_predicate(inv)
+
+sys.add_clause(head=Apply(inv, [Int(0)]))
+
+x = Symbol("x", INT)
+nx = Symbol("nx", INT)
+
+sys.add_clause(
+    body=And(Apply(inv, [x]), Equals(nx, Plus(x, Int(1)))),
+    head=Apply(inv, [nx])
+)
+sys.add_clause(
+    body=And(Apply(inv, [x]), LT(x, Int(3))),
+    head=FALSE()
+)
+
+# Serialize the CHC system to an SMT-LIBv2 file
+tmp = Path("chc_example.smt2")
+sys.serialize(tmp)
+print("written to:", tmp.resolve())
+
+# TODO: use a factory
+# Note: this requires Golem to be installed and accessible in PATH
+# otherwise, set `binary_path` argument to the GolemSolver constructor
+solver = GolemSolver()
+solver.load_system(sys)
+status = solver.solve(get_witness=True, proof_format=ProofFormat.ALETHE)
+print(f"Solving status: {status}")
+
+witness = solver.get_witness()
+
+assert status == Status.UNSAT and witness is not None
+    
+print(f"UNSAT witness:\n{witness.text}")
