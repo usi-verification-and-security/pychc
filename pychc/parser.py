4ac53a01b417a140838f57e208e448327ea28ec2
Anna Becchi <lanna.becchi@gmail.com>
2025-12-22 20:57:58 +0100

    Avoid warnings when setting Horn logic

--- pychc/parser.py
+++ pychc/parser.py
@@ -1,6 +1,8 @@
 import functools
 
-from pysmt.smtlib.parser import SmtLibParser
+from pysmt.smtlib.parser import SmtLibParser, SmtLibCommand, warn
+from pysmt.exceptions import UndefinedLogicError
+from pysmt.logics import get_logic_by_name
 from pysmt.fnode import FNode
 
 
@@ -15,6 +17,20 @@ class CHCSmtLibParser(SmtLibParser):
         mgr = self.env.formula_manager
         self.interpreted["div"] = self._operator_adapter(mgr.IntDiv)
         self.interpreted["mod"] = self._operator_adapter(mgr.Mod)
+
+    def _cmd_set_logic(self, current, tokens):
+        """(set-logic <symbol>)"""
+        elements = self.parse_atoms(tokens, current, 1)
+        name = elements[0]
+        if name == "HORN":
+            # Avoid warnings when reading a HORN logic file
+            return SmtLibCommand(current, [None])
+        try:
+            self.logic = get_logic_by_name(name)
+            return SmtLibCommand(current, [self.logic])
+        except UndefinedLogicError:
+            warn("Unknown logic '" + name + "'. Ignoring set-logic command.")
+            return SmtLibCommand(current, [None])
 
     def _reset(self):
         super()._reset()
